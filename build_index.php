e95942965fe3ce1748707d0d6573c91521363a01
Xianying7 <148400946+Xianying7@users.noreply.github.com>
2024-01-07 10:24:38 +0700

    update build_index.php

--- build_index.php
+++ build_index.php
@@ -456,6 +456,17 @@ function movePage(){
      ];
 }
 
+function remove_emoji($string) {
+    $symbols = "\x{1F100}-\x{1F1FF}"
+        ."\x{1F300}-\x{1F5FF}"
+        ."\x{1F600}-\x{1F64F}"
+        ."\x{1F680}-\x{1F6FF}"
+        ."\x{1F900}-\x{1F9FF}"
+        ."\x{2600}-\x{26FF}"
+        ."\x{2700}-\x{27BF}";
+
+    return preg_replace('/['. $symbols . ']+/u', '', $string);
+}
 
 function trimed($txt) {
     return preg_replace('/\s+/', '', $txt);
